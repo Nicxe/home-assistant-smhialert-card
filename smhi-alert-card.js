78aaf547e20da3664d7ebb0550f9f04df80fc06c
Niklas V <niklas.vilnersson@gmail.com>
2025-06-15 18:39:50 +0200

    Revert "Update icon paths"

--- smhi-alert-card.js
+++ smhi-alert-card.js
@@ -1,11 +1,11 @@
 import { LitElement, html, css } from 'https://unpkg.com/lit?module';
 
-const fireIcon = new URL('./icons/fire.svg', import.meta.url).href;
-const waterShortageIcon = new URL('./icons/waterShortage.svg', import.meta.url).href;
-const temperatureIcon = new URL('./icons/temperature.svg', import.meta.url).href;
-const yellowWarningIcon = new URL('./icons/yellowWarning.svg', import.meta.url).href;
-const orangeWarningIcon = new URL('./icons/orangeWarning.svg', import.meta.url).href;
-const redWarningIcon = new URL('./icons/redWarning.svg', import.meta.url).href;
+const fireIcon = new URL('./fire.svg', import.meta.url).href;
+const waterShortageIcon = new URL('./waterShortage.svg', import.meta.url).href;
+const temperatureIcon = new URL('./temperature.svg', import.meta.url).href;
+const yellowWarningIcon = new URL('./yellowWarning.svg', import.meta.url).href;
+const orangeWarningIcon = new URL('./orangeWarning.svg', import.meta.url).href;
+const redWarningIcon = new URL('./redWarning.svg', import.meta.url).href;
 
 class SmhiAlertCard extends LitElement {
   static properties = {
